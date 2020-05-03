f928bc48420ddc2df80529af3af3d9057438822b
Rushi Vishavadia <rushi.v@gmail.com>
2020-05-03 19:13:14 +0530

    Initial backup commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+    <title>Rushi Vishavadia</title>
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen" title="no title" charset="utf-8">
+    <script language="javascript">
+    // Munge up email address to prevent spambots harvesting it. 
+    function odQDmRTWjrpoAAJ() {
+        var KvtJJyQbzewivgX=["114","x75","115","x68","x69","46","118","x40","103","109","97","105","x6c","46","x63","x6f","x6d"]; var SRsmnvbdRMEpVLZ=[" "]; var suPYizQZbBauMHC=["114","117","115","104","105","x2e","118","x40","103","109","x61","x69","108","46","x63","x6f","109"]; document.write("<a href=\"&#x6d;&#97;&#x69;&#x6c;&#x74;&#111;&#x3a;");
+        for (i=0; i<KvtJJyQbzewivgX.length; i++) document.write("&#"+KvtJJyQbzewivgX[i]+";"); for (i=0; i<SRsmnvbdRMEpVLZ.length; i++) document.write(SRsmnvbdRMEpVLZ[i]); document.write("\">");
+        for (i=0; i<suPYizQZbBauMHC.length; i++) document.write("&#"+suPYizQZbBauMHC[i]+";"); document.write('</a>');
+    }
+    function qwKeFfEXZLNYduv() {
+        var uhtKlGeFBmoTJNT=["x72","117","x73","x68","105","46","x76","x40","x67","109","x61","105","108","46","99","111","109"]; var ZCgQXLMxUDWRPdf=[" "]; var zaFOEEEbofQHfUN=["114","x75","x73","x68","x69","46","118","64","103","x6d","x61","x69","108","x2e","x63","111","109"]; document.write("<span class='sn_url'>");
+        for (i=0; i<zaFOEEEbofQHfUN.length; i++) document.write("&#"+zaFOEEEbofQHfUN[i]+";"); document.write('</span>');
+    }
+    function TMpHSGAcSwtbEqS() {
+        var buuNRBjHchIiJiX=["114","x75","118","105","115","104","x40","104","x6f","116","109","x61","x69","108","46","x63","111","x6d"];var xMtLhGPDHhUdxnr=[" "];var IXQwmFmFJzvrBYW=["114","117","x76","105","x73","104","64","104","111","116","x6d","97","x69","108","x2e","x63","111","109"];document.write("<span class='sn_url'>");
+        for (i=0; i<IXQwmFmFJzvrBYW.length; i++) document.write("&#"+IXQwmFmFJzvrBYW[i]+";"); document.write('</span>');
+    }
+    </script>
+  <?php @include_once("stats.php"); ?>
+</head>
+<body>
+<div id='center'>
+    <div class='name'>
+        Rushi Vishavadia
+    </div>
+    <hr size='1' />
+    <div id="content">  
+        <!-- <p>Web Developer, Linux Enthusiast</p> -->
+        <p><strong>Email</strong>&nbsp;&nbsp;<script language="javascript">odQDmRTWjrpoAAJ();</script></p>
+        <p>
+            <strong>Web</strong>
+            <ul id='websites'>
+                <li><a href="http://rushi.vishavadia.com/blog/" title="Rushi's Ramblings">My Blog</a></li>
+                <li><a href="http://rushi.tumblr.com" title="Rushi MK-II">My Tumblelog</a></li>
+                <li><a href="http://rushi.posterous.com" title="Rushi's posterous - Random photos, posts and other tomfoolery.">My Posterous</a></li>
+            </ul>
+        </p>
+        <div class="networks">
+            <p class='iconify' id='gtalk'>
+                <a href="gtalk:rushi.v@gmail.com" title="Google Talk - rushi.v">Google Talk</a><br>
+                <script language="javascript">qwKeFfEXZLNYduv();</script>
+            </p>
+            <p class='iconify' id='msn'>
+                <a href="msn:ruvish@hotmail.com" title="MSN Messenger ruvish">MSN Messenger</a><br>
+                <script language="javascript">TMpHSGAcSwtbEqS();</script>
+            </p>
+        </div>
+        <br><br>
+        <div class="networks">
+            <p class='iconify' id='facebook'>
+                <a href="http://www.facebook.com/rushi.v" title="Facebook - Rushi">Facebook</a><br>
+                <span class='sn_url'>facebook.com/rushi.v</span>
+            </p>
+            <p class='iconify' id='skype'>
+                <a href="skype:rushi_v" title="Skype rushi_v">Skype</a><br>
+                <span class='sn_url'>Skype:rushi_v</span>
+            </p>
+        </div>
+        <br><br>
+        <div class="networks">
+            <p class='iconify' id='twitter'>
+                <a href="http://twitter.com/rushiv" title="Twitter @rushiv">Twitter</a><br>
+                <span class='sn_url'>twitter.com/rushiv</span>
+            </p>
+            <p class='iconify' id='friendfeed'>
+                <a href="http://friendfeed.com/rushi" title="friendfeed rushi">FriendFeed</a><br>
+                <span class='sn_url'>friendfeed.com/rushi</span>
+            </p>
+        </div>
+    </div>
+</div>
+<div class='bl'> &nbsp; </div>
+<script type="text/javascript" src="http://www.observerapp.com/record.js"></script>
+<script type="text/javascript">
+try { Observerapp.record("4c907563e150055046005cc8"); } catch(e) {};
+</script>
+</body>
+</html>
